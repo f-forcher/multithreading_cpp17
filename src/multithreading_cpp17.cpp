fbbf5d0074c2712bf413bcc5916e9f6abcfaa410
fforcher <francesco.forcher@gmail.com>
2016-11-22 12:14:29 +0100

    First code

---
+++ src/multithreading_cpp17.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : multithreading_cpp17.cpp
+// Author      : Francesco Forcher
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
